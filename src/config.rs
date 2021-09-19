78337f72b11cacbeeb1456c069d343a5884254c5
tim <tmsteem82@gmail.com>
2021-09-19 22:26:23 +0200

    implement config for triangle arbitrage data, increased log file size as we are logging more now

---
+++ src/config.rs
@@ -0,0 +1,15 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct TriangleConfig {
+    pub parts: [String; 3],
+    pub pairs: [String; 3],
+}
+
+#[derive(Debug, Deserialize)]
+pub struct AppConfig {
+    pub update_interval: u32,
+    pub results_limit: u32,
+    pub depth_streams: Vec<String>,
+    pub triangles: Vec<TriangleConfig>,
+}
